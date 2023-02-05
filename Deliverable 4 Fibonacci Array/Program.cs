756c1336523d3856476488c3d22bb1b7e508939d
Mauricio Bermudez <bermudezm@usf.edu>
2023-02-05 17:30:16 -0500

    Add project files.

---
+++ Deliverable 4 Fibonacci Array/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Deliverable_4_Fibonacci_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fib;
+            int a = 0, b = 1, c = 0;
+            Console.Write("{0} {1}", a, b);
+
+            for (int i = 2; i < 25; i++)
+            {
+                c = a + b;
+                Console.Write(" {0}", c);
+                a = b;
+                b = c;
+            }            
+        }
+    }
+}
